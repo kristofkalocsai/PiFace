d7ca4feffa8fa1ea982c686664be2a3615b25dcd
kristofkalocsai <kristofkalocsai@gmail.com>
2016-05-26 22:06:31 +0200

    pin 25 now working
    damn indexing
    readAll() in button handlers hangs up gui, needs work

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtNetwork>
+
+#define HOST "pizero.local"
+#define PORT 3344
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+//    w.show();
 
+    w.connectTCP();
+    w.readAllPins();
+
+    w.show();
     return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpSocket>
+#include <QTextStream>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +16,27 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void connectTCP();
+    void readTCPData();
+    void writeTCPData();
+    void readAllPins();
+    void pinToggle(int pin);
+    void closeEvent(QCloseEvent *event);
+    void setBgColorFromPin(int pin,int lvl);
+
+private slots:
+
+    void on_pushButton_clicked();
+
+    void on_pin_37_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QTcpSocket *socket;
+    QByteArray pins;
+    char gpios[31];
+    QTextStream out();
+
 };
 
 #endif // MAINWINDOW_H
